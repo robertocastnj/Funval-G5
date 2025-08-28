0e0762abd5a3b3af487c3e1641248d8b562dc6cb
Roberto Castillo <roberto.castnj@gmail.com>
2025-08-27 18:40:38 -0600

    Agregaron camcios pc casa

---
+++ Clase-13/Mañana/main.js
@@ -0,0 +1,15 @@
+let n_1 = parseFloat(prompt('Ingrese un número a multiplicar entero positivo:'))
+let n_2 = parseFloat(
+  prompt('Ingrese otro número a multiplicar entero positivo:')
+)
+if (Number.isInteger(n_1) && n_1 > 0 && Number.isInteger(n_2) && n_2 > 0) {
+  let res = n_2
+  for (i = 1; i < n_1; i++) {
+    res = res + n_2
+  }
+  alert(
+    'El resultado de multiplicar ' + n_1 + ' por ' + n_2 + ' es igual a ' + res
+  )
+} else {
+  alert('No es un número entero positivo')
+}
