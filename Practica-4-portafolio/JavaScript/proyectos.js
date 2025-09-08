bb8f3a26b60ffc2a7e587603de9a685026196e05
Roberto Castillo <roberto.castnj@gmail.com>
2025-09-08 08:14:50 -0600

    Se agregaron cambios de portafolio

--- Practica-4-portafolio/JavaScript/proyectos.js
+++ Practica-4-portafolio/JavaScript/proyectos.js
@@ -1,24 +1,24 @@
 let projects = [
   {
-    image: './images/proyectos/steam.png',
+    image: './images/proyectos/steam.PNG',
     name: 'Steam',
     description: `A copy of the Steam login page was made.`,
     link: `https://robertocastnj.github.io/Funval-G5/Practica-2/index.html`,
   },
   {
-    image: './images/proyectos/calculadora.png',
+    image: './images/proyectos/calculadora.PNG',
     name: 'Calculator',
     description: `An area calculator for geometric figures was created, where data is entered, and a result is provided.`,
     link: `https://robertocastnj.github.io/Funval-G5/Practica-5/index.html`,
   },
   {
-    image: './images/proyectos/banco.png',
+    image: './images/proyectos/banco.PNG',
     name: 'Bank',
     description: `A programmable banking logic was developed, allowing deposits, withdrawals, and balance inquiries from an initial account.`,
     link: `https://robertocastnj.github.io/Funval-G5/Practica-6/index.html`,
   },
   {
-    image: './images/proyectos/tictac.png',
+    image: './images/proyectos/tictac.PNG',
     name: 'Tic Tac Toe',
     description: `The design of the Tic Tac Toe game was created.`,
     link: `https://robertocastnj.github.io/Funval-G5/Practica-Tic-Tac-Toe/index.html`,
