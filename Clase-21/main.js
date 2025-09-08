03f914c809968eda6904dac65f8e91e12f85feae
Roberto Castillo <roberto.castnj@gmail.com>
2025-09-08 10:53:52 -0600

    Cambios en el día 21

--- Clase-21/main.js
+++ Clase-21/main.js
@@ -1,4 +1,4 @@
-function ejecutarFuncion(funcion) {
+/* function ejecutarFuncion(funcion) {
   funcion()
 }
 
@@ -11,3 +11,29 @@ ejecutarFuncion(saludar)
 ejecutarFuncion(() => {
   console.log('¡Hola mundo!')
 })
+ */
+
+const estudiantes = [
+  { nombre: 'Ana', edad: 20, nota: 85 },
+  { nombre: 'Luis', edad: 22, nota: 92 },
+  { nombre: 'Carlos', edad: 21, nota: 78 },
+  { nombre: 'Marta', edad: 20, nota: 88 },
+]
+
+const evaluados = estudiantes.map((est) => ({
+  ...est,
+  estado: est.nota >= 80 ? 'Aprobado' : 'Reprobado',
+}))
+
+console.log(evaluados)
+
+const conteoPorEdad = estudiantes.reduce((acc, est) => {
+  acc[est.edad] = (acc[est.edad] || 0) + 1
+  return acc
+}, {})
+
+console.log(conteoPorEdad)
+
+const perfecto = estudiantes.find((est) => est.nota === 100)
+
+console.log(perfecto)
