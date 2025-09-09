b546754968c090a94fed24dae583aea9684c6969
Roberto Castillo <roberto.castnj@gmail.com>
2025-09-08 21:28:18 -0600

    Cambios tic tac

--- Practica-Tic-Tac-Toe/Javascript/main.js
+++ Practica-Tic-Tac-Toe/Javascript/main.js
@@ -1,37 +1,37 @@
 /* ------------ Player 1 selection & type of game -------------------------- */
-import {
-  setActive,
-  validateSelection,
-  btnTurn,
-  xmark,
-  omark,
-} from "./funciones.js";
+import { setActive, validateSelection, positionClick } from './funciones.js'
 
-if (window.location.pathname.endsWith("index.html")) {
-  localStorage.clear();
-  const xBtn = document.getElementById("xButton");
-  const oBtn = document.getElementById("oButton");
-  const cpuButton = document.getElementById("cpu_btn");
-  const playerButton = document.getElementById("player_btn");
+if (window.location.pathname.endsWith('index.html')) {
+  localStorage.clear()
+  const xBtn = document.getElementById('xButton')
+  const oBtn = document.getElementById('oButton')
+  const cpuButton = document.getElementById('cpu_btn')
+  const playerButton = document.getElementById('player_btn')
 
-  xBtn.addEventListener("click", () => setActive(xBtn, oBtn, "X"));
-  oBtn.addEventListener("click", () => setActive(oBtn, xBtn, "O"));
+  xBtn.addEventListener('click', () => setActive(xBtn, oBtn, 'X'))
+  oBtn.addEventListener('click', () => setActive(oBtn, xBtn, 'O'))
 
-  playerButton.addEventListener("click", (e) => validateSelection(e, "player"));
-  cpuButton.addEventListener("click", (e) => validateSelection(e, "cpu"));
+  playerButton.addEventListener('click', (e) => validateSelection(e, 'player'))
+  cpuButton.addEventListener('click', (e) => validateSelection(e, 'cpu'))
 }
 
-if (window.location.pathname.endsWith("game.html")) {
-  let n = 4;
-  btnTurn(n);
-  const pos1 = document.getElementById("bt1");
-  if (n % 2 === 0) {
-    pos1.addEventListener("click", () => omark(pos1));
-    n++;
-    console.log(n);
-  } else {
-    pos1.addEventListener("click", () => xmark(pos1));
-    n++;
-    console.log(n);
+if (window.location.pathname.endsWith('game.html')) {
+  let turn = 0
+  const totalPositions = 9
+  let board = [0, 0, 0, 0, 0, 0, 0, 0, 0]
+
+  /* Player vs Player */
+  for (let i = 1; i <= totalPositions; i++) {
+    const btn = document.getElementById(`bt${i}`)
+    btn.addEventListener('click', () => {
+      const result = positionClick(i, btn, board, turn)
+      board = result.occupied
+      turn = result.turn
+      console.log(turn)
+      console.log(board)
+      if (turn >= 9) {
+        alert('El juego acabo')
+      }
+    })
   }
 }
